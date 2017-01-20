d962a750394630bc5a16ca3f2056138b3d0c06a1
yassu0320 <mathyassu@gmail.com>
2017-01-21 03:21:53 +0900

    get repoinfo

--- github_viewer_slack/github_json_maker/add.py
+++ github_viewer_slack/github_json_maker/add.py
@@ -4,7 +4,8 @@
 from slackbot.bot import respond_to
 import re
 
-@respond_to('add', re.IGNORECASE)
-def add(message):
+@respond_to('^\s*add\s+(\S+)\s*$', re.IGNORECASE)
+def add(message, github_url):
     text = message.body['text']
+    user_name, repo_name = github_url.split('/')
     message.reply('You send "{}"'.format(text))
