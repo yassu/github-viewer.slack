79d69cfade42dcef4ed93fc0fc7c50082e682001
yassu0320 <mathyassu@gmail.com>
2017-01-22 19:17:26 +0900

    rmができるようにする

--- github_viewer_slack/github_json_maker/rm.py
+++ github_viewer_slack/github_json_maker/rm.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from slackbot.bot import respond_to
+from slackbot_settings import COMMITS_JSON_FILENAME
 import json
 
 
@@ -11,4 +12,15 @@ def rm(message, github_url):
         message.reply('Illegal as github repository')
         return
     user_name, repo_name = github_url.split('/')
+    delete_repo(user_name, repo_name)
     message.reply("rm message")
+
+
+def delete_repo(user_name, repo_name):
+    d = dict()
+    with open(COMMITS_JSON_FILENAME) as jf:
+        d = json.load(jf)
+    del(d[user_name][repo_name])
+
+    with open(COMMITS_JSON_FILENAME, 'w') as jf:
+        json.dump(d, jf, indent=4)
