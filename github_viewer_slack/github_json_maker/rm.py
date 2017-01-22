92cf895b472ebf26ce05d7605dc2d31abcb31b4e
yassu0320 <mathyassu@gmail.com>
2017-01-22 19:31:01 +0900

    レポジトリが既に登録されているときエラーを返す

--- github_viewer_slack/github_json_maker/rm.py
+++ github_viewer_slack/github_json_maker/rm.py
@@ -3,6 +3,7 @@
 
 from slackbot.bot import respond_to
 from slackbot_settings import COMMITS_JSON_FILENAME
+from github_json_maker.utils import NotFoundRepositoryException
 import json
 
 
@@ -12,15 +13,25 @@ def rm(message, github_url):
         message.reply('Illegal as github repository')
         return
     user_name, repo_name = github_url.split('/')
-    delete_repo(user_name, repo_name)
-    message.reply("rm message")
+    try:
+        delete_repo(user_name, repo_name)
+    except NotFoundRepositoryException as ex:
+        message.reply('{}/{}は既に登録されています'.format(
+            user_name, repo_name))
+        return
+
+    message.reply("{}/{} を削除しました".format(user_name, repo_name))
 
 
 def delete_repo(user_name, repo_name):
     d = dict()
     with open(COMMITS_JSON_FILENAME) as jf:
         d = json.load(jf)
-    del(d[user_name][repo_name])
+    try:
+        del(d[user_name][repo_name])
+    except KeyError:
+        raise NotFoundRepositoryException('{}/{}は既に登録されています'.format(
+            user_name, repo_name))
 
     with open(COMMITS_JSON_FILENAME, 'w') as jf:
         json.dump(d, jf, indent=4)
