e2888e9170fb92a9acd754eddf227597a5168b24
yassu0320 <mathyassu@gmail.com>
2017-01-24 22:57:39 +0900

    error_log

--- github_viewer_slack/utils.py
+++ github_viewer_slack/utils.py
@@ -2,7 +2,33 @@
 # -*- coding: utf-8 -*-
 from urllib.request import urlopen
 from urllib.error import HTTPError
+from datetime import datetime
+import functools
+import traceback
 import json
+
+LOG_FILENAME = 'sample.log'
+TRACEBACK_LIMIT = 10
+
+
+def my_error_wrap(filename=LOG_FILENAME):
+    def _my_error_wrap(func):
+        @functools.wraps(func)
+        def command_func(*args, **kw):
+            try:
+                return func(*args, **kw)
+            except Exception as ex:
+                with open(filename, 'a') as f:
+                    now = datetime.now()
+                    year, mon, day = now.year, now.month, now.day
+                    hour, minute, second = now.hour, now.minute, now.second
+                    print(hour)
+                    f.write("{} {}/{} {}:{}:{}\n".format(year, mon, day,
+                                                        hour, minute, second))
+                    traceback.print_exc(TRACEBACK_LIMIT, f)
+                    f.write('\n')
+        return command_func
+    return _my_error_wrap
 
 
 def get_last_commit_id(user_name, repo_name):
