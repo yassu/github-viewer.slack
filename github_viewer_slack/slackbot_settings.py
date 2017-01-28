477181b8efb2c7ba235345896d3c5a455ec19fc0
yassu0320 <mathyassu@gmail.com>
2017-01-28 21:28:31 +0900

    Channel setting by CONFIG_DATA

--- github_viewer_slack/slackbot_settings.py
+++ github_viewer_slack/slackbot_settings.py
@@ -5,11 +5,14 @@ import json
 
 CONFIG_FILENAME = './../config.json'
 COMMITS_JSON_FILENAME = 'commits.json'
+CONFIG_DATA = json.load(open(CONFIG_FILENAME))
+
 ERROR_LOG_FILENAME = 'log/error_log'
 LOG_FILENAME = 'log/log'
 TRACEBACK_LIMIT = 10
 API_TOKEN = json.load(open(CONFIG_FILENAME))['API_TOKEN']
-Channel = 'general'
+API_TOKEN = CONFIG_DATA['API_TOKEN']
+Channel = CONFIG_DATA['CHANNEL']
 
 PLUGINS = [
     'github_json_maker',
