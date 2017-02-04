ee65301387869ec16b7ed31acbcadacf9f726050
yassu0320 <mathyassu@gmail.com>
2017-02-04 16:58:53 +0900

    list command with pattern

--- github_viewer_slack/github_json_maker/list.py
+++ github_viewer_slack/github_json_maker/list.py
@@ -7,10 +7,16 @@ from slackbot.bot import respond_to
 from utils import get_commits, get_branch_names, my_error_wrap
 
 
-@respond_to('^\s*list\s*$', re.IGNORECASE)
+@respond_to('^\s*list(\s+\S+)?\s*$', re.IGNORECASE)
 @my_error_wrap()
-def list(message):
+def list(message, pattern):
+    if pattern is None:
+        pattern = '.*'
+    pattern = pattern.strip()
+    print(pattern)
+
     text = "branch names\n"
     for branch_name in get_branch_names():
-        text += "* {}\n".format(branch_name)
+        if re.search(pattern, branch_name):
+            text += "* {}\n".format(branch_name)
     message.reply(text)
