3b78aa63d3bf8d69ed88e3fe51c389c2182fd51f
yassu0320 <mathyassu@gmail.com>
2017-02-04 16:35:40 +0900

    help syntax

--- github_viewer_slack/github_json_maker/help.py
+++ github_viewer_slack/github_json_maker/help.py
@@ -6,11 +6,27 @@ from slackbot.bot import respond_to
 from slackbot_settings import COMMITS_JSON_FILENAME, TRACEBACK_LIMIT
 from utils import COMMAND_TO_HELP
 
-@respond_to('^\s*help\s+(\S+)\s*$', re.IGNORECASE)
+@respond_to('^\s*help(\s+\S+)?\s*$', re.IGNORECASE)
 def help(message, command_name):
+    if command_name is None:
+        help_str = get_help_without_argument()
+    else:
+        command_name = command_name.strip()
+        help_str = get_help_with_argument(command_name)
+    message.reply(help_str)
+
+
+def get_help_without_argument():
+    help_str = '\n'
+    for _, help_ in COMMAND_TO_HELP.items():
+        help_str += '* {}\n'.format(help_)
+    return help_str
+
+
+def get_help_with_argument(command_name):
     help_str = ''
     if command_name in COMMAND_TO_HELP:
         help_str = COMMAND_TO_HELP[command_name]
     else:
         help_str = '{} is not used.'.format(command_name)
-    message.reply(help_str)
+    return help_str
